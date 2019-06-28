afa6521f38cca05fa460c5c09813ccaa9905d3b9
AndreAFR <andref.rosemann@gmail.com>
2019-06-28 18:43:58 -0300

    commit

--- AndreEFelipe/src/br/com/lista1/Questao12.java
+++ AndreEFelipe/src/br/com/lista1/Questao12.java
@@ -14,17 +14,30 @@ public class Questao12 {
 		num4 = Teclado.lerInt("Digite um numero: ");
 		num5 = Teclado.lerInt("Digite um numero: ");
 		
-		if(num1 > num2) {
+		if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
 			maior = num1;
-		} else if(num2 > num3) {
+		} else if(num2 > num3 && num2 > num4 && num2 > num5) {
 			maior = num2;
-		} else if(num3 > num4) {
+		} else if(num3 > num4 && num3 > num5) {
 			maior = num3;
 		} else if(num4 > num5) {
 			maior = num4;
 		} else {
 			maior = num5;
 		}
+		
+		if(num1 < num2 && num1 < num3 && num1 < num4 && num1 < num5) {
+			menor = num1;
+		} else if(num2 < num3 && num2 < num4 && num2 < num5) {
+			menor = num2;
+		} else if(num3 < num4 && num3 < num5) {
+			menor = num3;
+		} else if(num4 < num5) {
+			menor = num4;
+		} else {
+			menor= num5;
+		}
 		System.out.println("Maior: " + maior);
+		System.out.println("Menor: " + menor);
 	}
 }
